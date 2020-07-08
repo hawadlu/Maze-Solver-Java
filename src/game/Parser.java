d7d3f1257591ccc2e933d0d2889e9807033f4b8e
Luke <hawadlu@outlook.com>
2020-07-09 10:17:01 +1200

    Begins programming the game.

---
+++ src/game/Parser.java
@@ -0,0 +1,13 @@
+package game;
+
+import java.util.Scanner;
+
+/**
+ * This class contains all of the methods needed to parse the program.
+ */
+public class Parser {
+    //Parse the program
+    public void parseProgram(Scanner scanner) {
+
+    }
+}
